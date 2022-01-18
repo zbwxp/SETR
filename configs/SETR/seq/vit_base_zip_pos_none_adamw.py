971f4cdadbb28a6d35a0a13ecab5b44ca2fedac9
zhangbowen17 <zhangbowen17@meituan.com>
2022-01-18 21:44:41 +1030

    add pos none config

--- configs/SETR/seq/vit_base_zip_pos_none_adamw.py
+++ configs/SETR/seq/vit_base_zip_pos_none_adamw.py
@@ -83,7 +83,6 @@ optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), we
                  paramwise_cfg=dict(custom_keys={'pos_block': dict(decay_mult=0.),
                                                  'norm': dict(decay_mult=0.),
                                                  'head': dict(lr_mult=10.),
-                                                 'zip': dict(lr_mult=10.),
                                                  }))
 
 lr_config = dict(_delete_=True, policy='poly',
