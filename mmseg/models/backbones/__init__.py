4d608fdd3ff8644beb10bd03125d48c803e72da1
zhangbowen17 <zhangbowen17@meituan.com>
2022-01-21 19:29:02 +1030

    VT versions

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -8,6 +8,7 @@ from .vit import VisionTransformer
 from .vit_mla import VIT_MLA
 from .vit_bottle import Vit_Bottleneck
 from .vit_downsample import Vit_Downsample
+from .vit_VT import Vit_VT
 
 __all__ = [
     'VisionTransformer', 'VIT_MLA'
---
+++ mmseg/models/backbones/vit_VT.py
@@ -0,0 +1,105 @@
+# from .layers import DropPath, to_2tuple, trunc_normal_
+import copy
+import math
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from .vit import VisionTransformer
+from ..builder import BACKBONES
+
+
+class FilterTokenizer(nn.Module):
+    """
+    The Filter Tokenizer extracts visual tokens using point-wise convolutions.
+    It takes input of size (HW, C) and outputs a tensor of size (L, D) where:
+    - HW : height x width, which represents the number of pixels
+    - C : number of input channels
+    - L : number of tokens
+    - D : number of token channels
+    """
+
+    def __init__(self, in_channels: int, token_channels: int, tokens: int) -> None:
+        super(FilterTokenizer, self).__init__()
+
+        self.tokens = tokens
+        self.in_channels = in_channels
+        self.token_channels = token_channels
+
+        self.linear1 = nn.Linear(in_channels, tokens)
+        self.linear2 = nn.Linear(in_channels, token_channels)
+
+        self.cache1 = None
+        self.cache2 = None
+        self.token_cache = None
+
+        # initialize weights
+        nn.init.xavier_normal_(self.linear1.weight)
+        nn.init.xavier_normal_(self.linear2.weight)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Expected Input Dimensions: (N, HW, C), where:
+        - N: batch size
+        - HW: number of pixels
+        - C: number of input feature map channels
+
+        Expected Output Dimensions: (N, L, D), where:
+        - L: number of tokens
+        - D: number of token channels
+        """
+
+        a = self.linear1(x)  # of size (N, HW, L)
+        self.cache1 = a
+        a = a.softmax(dim=1)  # softmax for HW dimension, such that every group l features sum to 1
+        self.cache2 = a
+        a = torch.transpose(a, 1, 2)  # swap dimensions 1 and 2, of size (N, L, HW)
+        a = a.matmul(x)  # of size (N, L, C)
+        a = self.linear2(a)  # of size (N, L, D)
+
+        self.token_cache = a
+        return a
+
+
+@BACKBONES.register_module()
+class Vit_VT(VisionTransformer):
+    """ Vision Transformer with support for patch or hybrid CNN input stage
+    """
+
+    def __init__(self,
+                 VT_query=256,
+                 VT_channel=1,
+                 ziper_index=7,
+                 **kwargs):
+        super(Vit_VT, self).__init__(**kwargs)
+        self.ziper_index = ziper_index
+
+        token_layer = FilterTokenizer(
+            in_channels=kwargs['embed_dim'],
+            token_channels=kwargs['embed_dim'],
+            tokens=VT_query,
+        )
+        self.tokenizer = token_layer
+
+    def forward(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        x = x.flatten(2).transpose(1, 2)
+
+        # stole cls_tokens impl from Phil Wang, thanks
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        outs = []
+        for i, blk in enumerate(self.blocks):
+            if i == self.ziper_index:
+                x = x[:, 1:]
+                x = self.tokenizer(x)
+            x = blk(x)
+            if i in self.out_indices:
+                outs.append(x)
+        return tuple(outs)
