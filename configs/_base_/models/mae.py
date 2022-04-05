433b91228af8b5d55d6534742655f71851069b1f
zhangbowen17 <zhangbowen17@meituan.com>
2022-04-05 21:15:35 +0930

    add base model mae

---
+++ configs/_base_/models/mae.py
@@ -0,0 +1,33 @@
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='XCiT',
+        patch_size=16,
+        embed_dim=384,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=True,
+        use_abs_pos_emb=True,
+        use_rel_pos_bias=False,
+    ),
+    decode_head=dict(
+        type='VisionTransformerUpHead',
+        in_channels=1024,
+        channels=512,
+        in_index=23,
+        img_size=768,
+        embed_dim=1024,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        num_conv=2,
+        upsampling_method='bilinear',
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)))
+# model training and testing settings
+train_cfg = dict()
+test_cfg = dict(mode='whole')
